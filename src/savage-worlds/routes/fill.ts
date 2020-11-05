eba043ed679e86fb02e454d5548a54075cd1348d
Dmitry Kutsenko <d2emonium@gmail.com>
2020-11-05 10:19:52 +0300

    Fill edges & hindrances

--- src/savage-worlds/routes/fill.ts
+++ src/savage-worlds/routes/fill.ts
@@ -3,11 +3,19 @@ import {
     fillSkills,
 } from '../handlers/skill';
 import checkAdmin from '../middlewares/checkAdmin';
+import {
+    fillEdges,
+} from '../handlers/edge';
+import {
+    fillHindrances,
+} from '../handlers/hindrance';
 
 const router = express.Router();
 
 router.use(checkAdmin);
 
 router.post('/skills', fillSkills);
+router.post('/edges', fillEdges);
+router.post('/hindrances', fillHindrances);
 
 export default router;

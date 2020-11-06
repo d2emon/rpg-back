ba24a58db9ba1673a2700e9eecf851ad48f1dbb4
Dmitry Kutsenko <d2emonium@gmail.com>
2020-11-06 11:21:00 +0300

    Add edges

--- src/savage-worlds/fixtures/edges.ts
+++ src/savage-worlds/fixtures/edges.ts
@@ -43,9 +43,7 @@ const edges: IEdgeDocument[] = [
         group: 'background',
         requirements: {
             rank: 'novice',
-            edges: [
-                'arcane-resistance',
-            ],
+            edges: ['arcane-resistance'],
         },
     }),
     new Edge({
@@ -63,9 +61,7 @@ const edges: IEdgeDocument[] = [
         group: 'background',
         requirements: {
             rank: 'novice',
-            edges: [
-                'attractive',
-            ],
+            edges: ['attractive'],
         },
     }),
     new Edge({
@@ -115,9 +111,7 @@ const edges: IEdgeDocument[] = [
         group: 'background',
         requirements: {
             rank: 'novice',
-            edges: [
-                'luck',
-            ],
+            edges: ['luck'],
         },
     }),
     new Edge({
@@ -176,9 +170,7 @@ const edges: IEdgeDocument[] = [
         group: 'combat',
         requirements: {
             rank: 'veteran',
-            edges: [
-                'block',
-            ],
+            edges: ['block'],
         },
     }),
     new Edge({
@@ -204,9 +196,7 @@ const edges: IEdgeDocument[] = [
         group: 'combat',
         requirements: {
             rank: 'veteran',
-            edges: [
-                'dodge',
-            ],
+            edges: ['dodge'],
         },
     }),
     new Edge({
@@ -224,9 +214,7 @@ const edges: IEdgeDocument[] = [
         group: 'combat',
         requirements: {
             rank: 'heroic',
-            edges: [
-                'First Strike',
-            ],
+            edges: ['first-strike'],
         },
     }),
     new Edge({
@@ -267,9 +255,7 @@ const edges: IEdgeDocument[] = [
         group: 'combat',
         requirements: {
             rank: 'veteran',
-            edges: [
-                'frenzy',
-            ],
+            edges: ['frenzy'],
         },
     }),
     new Edge({
@@ -541,14 +527,390 @@ const edges: IEdgeDocument[] = [
     }),
 
     // Professional Edges
+    new Edge({
+        slug: 'ace',
+        name: 'Ace',
+        group: 'professional',
+        requirements: {
+            rank: 'novice',
+            agility: 'd8',
+        },
+    }),
+    new Edge({
+        slug: 'acrobat',
+        name: 'Acrobat',
+        group: 'professional',
+        requirements: {
+            rank: 'novice',
+            agility: 'd8',
+            strength: 'd6',
+        },
+    }),
+    new Edge({
+        slug: 'champion',
+        name: 'Champion',
+        group: 'professional',
+        requirements: {
+            rank: 'novice',
+            spirit: 'd8',
+            strength: 'd6',
+            vigor: 'd8',
+            edges: ['arcane-background'], // Miracles
+            skills: {
+                fighting: 'd8',
+                faith: 'd6',
+            },
+        },
+    }),
+    new Edge({
+        slug: 'gadgeteer',
+        name: 'Gadgeteer',
+        group: 'professional',
+        requirements: {
+            rank: 'novice',
+            smarts: 'd8',
+            strength: 'd6',
+            edges: ['arcane-background'], // Weird Science
+            skills: {
+                repair: 'd8',
+                'weird-science': 'd8',
+                // Two scientific knowledge d6
+            },
+        },
+    }),
+    new Edge({
+        slug: 'holy-warrior',
+        name: 'Holy/Unholy Warrior',
+        group: 'professional',
+        requirements: {
+            rank: 'novice',
+            spirit: 'd8',
+            edges: ['arcane-background'], // Miracles
+            skills: {
+                faith: 'd6',
+            },
+        },
+    }),
+    new Edge({
+        slug: 'investigator',
+        name: 'Investigator',
+        group: 'professional',
+        requirements: {
+            rank: 'novice',
+            smarts: 'd8',
+            skills: {
+                investigation: 'd8',
+                streetwise: 'd8',
+            },
+        },
+    }),
+    new Edge({
+        slug: 'jack-of-all-trades',
+        name: 'Jack-of-All-Trades',
+        group: 'professional',
+        requirements: {
+            rank: 'novice',
+            smarts: 'd10',
+        },
+    }),
+    new Edge({
+        slug: 'mc-gyver',
+        name: 'McGyver',
+        group: 'professional',
+        requirements: {
+            rank: 'novice',
+            smarts: 'd6',
+            skills: {
+                repair: 'd6',
+                notice: 'd8',
+            },
+        },
+    }),
+    new Edge({
+        slug: 'mentalist',
+        name: 'Mentalist',
+        group: 'professional',
+        requirements: {
+            rank: 'novice',
+            smarts: 'd8',
+            edges: ['arcane-background'], // Psionics
+            skills: {
+                psionics: 'd6',
+            },
+        },
+    }),
+    new Edge({
+        slug: 'mr-fix-it',
+        name: 'Mr. Fix It',
+        group: 'professional',
+        requirements: {
+            rank: 'novice',
+            smarts: 'd10',
+            edges: ['arcane-background'], // Weird Science
+            skills: {
+                repair: 'd8',
+                'weird-science': 'd8',
+                // Two scientific knowledge d6
+            },
+        },
+    }),
+    new Edge({
+        slug: 'scholar',
+        name: 'Scholar',
+        group: 'professional',
+        requirements: {
+            rank: 'novice',
+            skills: {
+                // Affected skill d8
+            },
+        },
+    }),
+    new Edge({
+        slug: 'thief',
+        name: 'Thief',
+        group: 'professional',
+        requirements: {
+            rank: 'novice',
+            agility: 'd8',
+            skills: {
+                climb: 'd6',
+                lockpick: 'd6',
+                stealth: 'd6',
+            },
+        },
+    }),
+    new Edge({
+        slug: 'wizard',
+        name: 'Wizard',
+        group: 'professional',
+        requirements: {
+            rank: 'novice',
+            smarts: 'd8',
+            edges: ['arcane-background'], // Magic
+            skills: {
+                knowledge: 'd8', // Arcana
+                spellcasting: 'd6',
+            },
+        },
+    }),
+    new Edge({
+        slug: 'woodsman',
+        name: 'Woodsman',
+        group: 'professional',
+        requirements: {
+            rank: 'novice',
+            spirit: 'd6',
+            skills: {
+                survival: 'd8',
+                tracking: 'd8',
+            },
+        },
+    }),
 
     // Social Edges
+    new Edge({
+        slug: 'charismatic',
+        name: 'Charismatic',
+        group: 'social',
+        requirements: {
+            rank: 'novice',
+            spirit: 'd8',
+        },
+    }),
+    new Edge({
+        slug: 'common-bond',
+        name: 'Common Bond',
+        group: 'social',
+        requirements: {
+            wildCard: true,
+            rank: 'novice',
+            spirit: 'd8',
+        },
+    }),
+    new Edge({
+        slug: 'connections',
+        name: 'Connections',
+        group: 'social',
+        requirements: {
+            rank: 'novice',
+        },
+    }),
+    new Edge({
+        slug: 'strong-willed',
+        name: 'Strong Willed',
+        group: 'social',
+        requirements: {
+            rank: 'novice',
+            skills: {
+                intimidate: 'd6',
+                taunt: 'd6',
+            },
+        },
+    }),
 
     // Weird Edges
+    new Edge({
+        slug: 'beast-bond',
+        name: 'Beast Bond',
+        group: 'weird',
+        requirements: {
+            rank: 'novice',
+        },
+    }),
+    new Edge({
+        slug: 'beast-master',
+        name: 'Beast Master',
+        group: 'weird',
+        requirements: {
+            rank: 'novice',
+            spirit: 'd8',
+        },
+    }),
+    new Edge({
+        slug: 'danger-sense',
+        name: 'danger Sense',
+        group: 'weird',
+        requirements: {
+            rank: 'novice',
+        },
+    }),
+    new Edge({
+        slug: 'healer',
+        name: 'Healer',
+        group: 'weird',
+        requirements: {
+            rank: 'novice',
+            spirit: 'd8',
+        },
+    }),
 
     // Wild Card Edges
+    new Edge({
+        slug: 'dead-shot',
+        name: 'Dead Shot',
+        group: 'wild-card',
+        requirements: {
+            wildCard: true,
+            rank: 'seasoned',
+            any: [
+                {
+                    skills: { shooting: 'd10' },
+                },
+                {
+                    skills: { throwing: 'd10' },
+                },
+            ],
+        },
+    }),
+    new Edge({
+        slug: 'mighty-blow',
+        name: 'Mighty Blow',
+        group: 'wild-card',
+        requirements: {
+            wildCard: true,
+            rank: 'seasoned',
+            skills: {
+                fighting: 'd10',
+            },
+        },
+    }),
+    new Edge({
+        slug: 'power-surge',
+        name: 'Power Surge',
+        group: 'wild-card',
+        requirements: {
+            wildCard: true,
+            rank: 'seasoned',
+            skills: {
+                'arcane-skill': 'd10',
+            },
+        },
+    }),
 
     // Legendary Edges
+    new Edge({
+        slug: 'followers',
+        name: 'Followers',
+        group: 'legendary',
+        requirements: {
+            wildCard: true,
+            rank: 'legendary',
+        },
+    }),
+    new Edge({
+        slug: 'professional',
+        name: 'Professional',
+        group: 'legendary',
+        requirements: {
+            rank: 'legendary',
+            // Affected trait 'd12
+        },
+    }),
+    new Edge({
+        slug: 'expert',
+        name: 'Expert',
+        group: 'legendary',
+        requirements: {
+            rank: 'legendary',
+            edges: ['professional'],
+        },
+    }),
+    new Edge({
+        slug: 'master',
+        name: 'Master',
+        group: 'legendary',
+        requirements: {
+            wildCard: true,
+            rank: 'legendary',
+            edges: ['expert'],
+        },
+    }),
+    new Edge({
+        slug: 'sidekick',
+        name: 'Sidekick',
+        group: 'legendary',
+        requirements: {
+            wildCard: true,
+            rank: 'legendary',
+        },
+    }),
+    new Edge({
+        slug: 'tough-as-nails',
+        name: 'Tough as Nails',
+        group: 'legendary',
+        requirements: {
+            rank: 'legendary',
+        },
+    }),
+    new Edge({
+        slug: 'improved-tough-as-nails',
+        name: 'Improved Tough as Nails',
+        group: 'legendary',
+        requirements: {
+            rank: 'legendary',
+            edges: ['tough-as-nails'],
+        },
+    }),
+    new Edge({
+        slug: 'weapon-master',
+        name: 'Weapon Master',
+        group: 'legendary',
+        requirements: {
+            rank: 'legendary',
+            skills: {
+                fighting: 'd12',
+            },
+        },
+    }),
+    new Edge({
+        slug: 'master-of-arms',
+        name: 'Master of Arms',
+        group: 'legendary',
+        requirements: {
+            rank: 'legendary',
+            edges: ['weapon-master'],
+        },
+    }),
 
 ];
 
